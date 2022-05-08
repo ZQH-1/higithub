58f501f959b5785f57cb25cc08cfe555b516dad1
zq13602874 <aq661444>
2022-05-08 21:19:57 +0800

    commit test

--- src/commit/testCommit.java
+++ src/commit/testCommit.java
@@ -4,5 +4,6 @@ public class testCommit {
     public static void main(String[] args) {
         System.out.println("commit success");
         System.out.println("2nd commit");
+        System.out.println("3rd");
     }
 }
